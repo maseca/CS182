c0374ab9cfd539b2f0f09389f3d13e1a76852991
maseca <maxwell.plotkin@metalab.csun.edu>
2017-09-09 13:12:53 -0700

    Fraction accounts for negatives

--- week02/Fraction.java
+++ week02/Fraction.java
@@ -4,17 +4,31 @@ public class Fraction{
     public int num, den;
 
     public Fraction(int n, int d){
-        while (n != 1 && d % n == 0){
-            d /= n;
-            n /= n;
-        }//while
+		int gcd;
+		int sign = 1;
 
-        this.num = n;
+		if(n < 0 && d < 0){
+			n *= -1;
+			d *= -1;
+		}else if(n < 0){
+			n *= -1;
+			sign = -1;
+		}else if(d < 0){
+			d *= -1;
+			sign = -1;
+		}
+
+		do{
+			gcd = PlotkinGCD.findGCD(n,d);
+			n /= gcd;
+			d /= gcd;
+		}while(gcd != 1);
+
+        this.num = n * sign;
         this.den = d;
     }//constructor
 
     public String display(){
         return this.num + "/" + this.den;
     }//display
-
 }//fraction
--- week02/Test.java
+++ week02/Test.java
@@ -2,7 +2,7 @@
 public class Test{
 
     public static void main(String[] args) {
-        Fraction f = new Fraction(2,4);
+        Fraction f = new Fraction(-2,4);
 
         System.out.println(f);
         System.out.println(f.num);
