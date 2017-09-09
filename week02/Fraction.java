cc1627fcec02f90d9a6c89b29c329d3bc3413cb2
maseca <maxwell.plotkin@metalab.csun.edu>
2017-09-09 13:45:49 -0700

    0-denom prints error and exits

--- week02/Fraction.java
+++ week02/Fraction.java
@@ -1,31 +1,41 @@
 //Maxwell Plotkin COMP182
+
 public class Fraction{
 
     public int num, den;
 
     public Fraction(int n, int d){
-		int gcd;
-		int sign = 1;
-
-		if(n < 0 && d < 0){
-			n *= -1;
-			d *= -1;
-		}else if(n < 0){
-			n *= -1;
-			sign = -1;
-		}else if(d < 0){
-			d *= -1;
-			sign = -1;
+		if(d == 0){
+			System.out.println("Denominator can not be 0");
+		    System.exit(1);
 		}
+		if(n == 0){
+			this.num = 0;
+			this.den = 1;
+		} else {
+			int gcd;
+			int sign = 1;
 
-		do{
-			gcd = PlotkinGCD.findGCD(n,d);
-			n /= gcd;
-			d /= gcd;
-		}while(gcd != 1);
+			if (n < 0 && d < 0) {
+				n *= -1;
+				d *= -1;
+			} else if (n < 0) {
+				n *= -1;
+				sign = -1;
+			} else if (d < 0) {
+				d *= -1;
+				sign = -1;
+			}
 
-        this.num = n * sign;
-        this.den = d;
+			do {
+				gcd = PlotkinGCD.findGCD(n, d);
+				n /= gcd;
+				d /= gcd;
+			} while (gcd != 1);
+
+			this.num = n * sign;
+			this.den = d;
+		}//else
     }//constructor
 
     public String display(){
