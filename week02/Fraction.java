681968b3c88953a0dabd68006ab2f9d73a85163c
maseca <maxwell.plotkin@metalab.csun.edu>
2017-09-09 12:04:24 -0700

    start of w2d2

---
+++ week02/Fraction.java
@@ -0,0 +1,10 @@
+public class Fraction{
+
+    public int num, denom;
+
+    Fraction(int n, int d){
+        this.num = n;
+        this.denom = d;
+    }
+
+}//fraction
---
+++ week02/Test.java
@@ -0,0 +1,10 @@
+public class Test{
+
+    public static void main(String[] args) {
+        Fraction f = new Fraction(2,4);
+        System.out.println(f);
+        System.out.println(f.num);
+        System.out.println(f.denom);
+    }//main
+
+}//test
