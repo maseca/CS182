49ae40fa424c1beaf1f980fa33d6ca8875e157d5
maseca <maxwell.plotkin@metalab.csun.edu>
2017-09-09 14:08:57 -0700

    simplified structure

--- week02/PlotkinGCD.java
+++ week02/PlotkinGCD.java
@@ -6,6 +6,17 @@ public class PlotkinGCD{
     }//main
 
     public static int findGCD(int x, int y){
+        if(x == 0 || y == 0){
+            System.out.println("0 has no common denominator");
+            System.exit(1);
+        }
+
+        if(x < 0)
+            x *= -1;
+
+        if(y < 0)
+            y *= -1;
+
         while(x != y){
             if(x > y)
                 x = x - y;
