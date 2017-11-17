bc0b5bd59ea5c0cff32db5c92b3ce1f6870def41
Maxwell Plotkin <plotkinmaxwell@gmail.com>
2017-11-17 11:54:58 -0800

    test cleaned up

--- SBTreeTest.java
+++ SBTreeTest.java
@@ -1,92 +1,43 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class SBTreeTest {
 	public static void main(String[] args) {
+		SBTree sbTree;
 		String in;
 		Scanner sc = new Scanner(System.in);
 
 		System.out.println("Input integers separated by spaces:");
 		try {
-			in = sc.nextLine().replaceAll("\\s{2,}"," ").trim();
+			in = sc.nextLine().replaceAll("\\s{2,}", " ").trim();
 		} catch (Exception e) {
 			System.err.println("No input detected.");
 			in = "0";
 		}
 
+		sbTree = treeFromStr(in);
+
 		System.out.print("Pre-Ordered: ");
-		System.out.println(treePre(in));
+		System.out.println(sbTree.toPreOrder());
 		System.out.print("In-Ordered: ");
-		System.out.println(treeIn(in));
+		System.out.println(sbTree.toInOrder());
 		System.out.print("Post-Ordered: ");
-		System.out.println(treePost(in));
-
-		//SortArray.insertion(ary);
-		//System.out.println("Sorted: " + Arrays.toPreOrder(ary));
-}
-
-	private static int[] aryFromStr(String inStr){
-		String[] strAry = inStr.split(" ");
-		int sLen = strAry.length;
-		int[] intAry = new int[sLen];
-
-		for(int i=0; i < sLen; ++i){
-			try {
-				intAry[i] = Integer.parseInt(strAry[i]);
-			}catch(Exception e){
-				System.err.println("Failed to parse: " + strAry[i]);
-			}
-		}
-
-		return intAry;
+		System.out.println(sbTree.toPostOrder());
 	}
 
-	private static String treePre(String inStr){
-		String[] strAry = inStr.split(" ");
-		SBTree tree = new SBTree();
-		int i = 0;
+	private static SBTree treeFromStr(String inStr){
+        String[] strAry = inStr.split(" ");
+        SBTree tree = new SBTree();
+        int i = 0;
 
 		for (String num : strAry) {
-			try {
-				i = Integer.parseInt(num);
-			} catch (Exception e) {
-				System.err.println("Failed to parse: " + num);
-			}
+            try { i = Integer.parseInt(num);
+            } catch (Exception e) {
+                System.err.println("Failed to parse: " + num);
+            }
 
-			tree.insert(i);
-		}
-		return tree.toPreOrder();
-	}
-	private static String treePost(String inStr){
-		String[] strAry = inStr.split(" ");
-		SBTree tree = new SBTree();
-		int i = 0;
+            tree.insert(i);
+        }
 
-		for (String num : strAry) {
-			try {
-				i = Integer.parseInt(num);
-			} catch (Exception e) {
-				System.err.println("Failed to parse: " + num);
-			}
-
-			tree.insert(i);
-		}
-		return tree.toPostOrder();
-	}
-	private static String treeIn(String inStr){
-		String[] strAry = inStr.split(" ");
-		SBTree tree = new SBTree();
-		int i = 0;
-
-		for (String num : strAry) {
-			try {
-				i = Integer.parseInt(num);
-			} catch (Exception e) {
-				System.err.println("Failed to parse: " + num);
-			}
-
-			tree.insert(i);
-		}
-		return tree.toInOrder();
-	}
+		return tree;
+    }
 }
